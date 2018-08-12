1a30f4d30d9ea13fd4859bc88b2011278c7ad711
marco baldi <marco.baldi@fing.edu.uy>
2018-08-12 16:01:49 -0300

    Creado ListaReproduccion con sus hijos

---
+++ src/uytube/logica/ListaReproduccion.java
@@ -0,0 +1,10 @@
+package uytube.logica;
+
+public abstract class ListaReproduccion {
+	protected String nombre;//Esto puede ser un error.
+	
+	public ListaReproduccion() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/uytube/logica/Particular.java
@@ -0,0 +1,11 @@
+package uytube.logica;
+
+public class Particular extends ListaReproduccion{
+	private boolean privado;
+	
+	public Particular(String n, boolean p) {
+		nombre = n;
+		privado = p;
+	}
+
+}
