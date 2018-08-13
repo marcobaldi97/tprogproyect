055903b292bf0c698424c5bcf9d74c8416bdafe8
santi <santiago.alaniz>
2018-08-13 19:20:16 -0300

    puse el ctrl y añadi la doblenavegabilidad de categoria

---
+++ src/uytube/logica/VideoCtrl.java
@@ -0,0 +1,12 @@
+package uytube.logica;
+
+public class VideoCtrl {
+	
+	DtListaReproduccion [] listarLDRPorCategoria(String cat) 
+	{
+		DtListaReproduccion [] resultado=null;
+		
+		return resultado;
+	}
+
+}
