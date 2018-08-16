5af51a8ce66cfc0dc18fb46ba178e9ec73ea995e
Nacho <Nacho@Ching>
2018-08-16 16:42:22 -0300

    la factory queda hecha

---
+++ src/uytube/logica/Factory.java
@@ -0,0 +1,22 @@
+package uytube.logica;
+
+public class Factory {
+	private static Factory instancia;
+	
+	private Factory(){
+		
+	}
+	public static Factory getInstance(){
+		if(instancia == null){
+			instancia= new Factory();
+		}
+		return instancia;
+	}
+	
+	public IUsuarioCtrl getIUsuarioCtrl(){
+		return UsuarioCtrl.getInstance();
+	}
+	public IVideoCtrl getIVideoCtrl(){
+		return VideoCtrl.getInstance();
+	}
+}
