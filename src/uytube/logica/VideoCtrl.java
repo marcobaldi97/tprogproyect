570816e800d1ce8d4a56cf1d5682f9344c75f663
Nacho <Nacho@Ching>
2018-08-16 16:17:11 -0300

    queda hecho el Controlador VideoCtrl (al menos de lo que tengo diagrama)

--- src/uytube/logica/VideoCtrl.java
+++ src/uytube/logica/VideoCtrl.java
@@ -1,16 +1,73 @@
 package uytube.logica;
 
 public class VideoCtrl {
+	private static VideoCtrl instance = null; 
 	
-	DtListaReproduccion [] listarLDRPorCategoria(String cat) 
+	private VideoCtrl() {
+	}
+	
+	public static VideoCtrl getInstance() {
+		if (instance == null) instance = new VideoCtrl();
+		return instance;
+	}
+	public DtListaReproduccion [] listarLDRPorCategoria(String cat) 
 	{
 		DtListaReproduccion [] resultado=null;
 		
 		return resultado;
 	}
+	public DtVideo[] listarVideosPorCategoria(String cat){
+		CategoriaHandler ch=CategoriaHandler.getInstance();
+		Categoria cate=ch.find(cat);
+		return cate.listarVideos();
+	}
 	public DtComentario[] listarComentarios(Integer IDVideo){
 		VideoHandler vh=VideoHandler.getInstance();
 		Video v=vh.find(IDVideo);
 		return v.getComentarios();
+	}
+	public void nuevoComentario(Integer IDVideo,String nickU,DtFecha fecha,String contenido){
+		VideoHandler vh=VideoHandler.getInstance();
+		Video v=vh.find(IDVideo);
+		v.nuevoComentario(nickU, fecha, contenido);
+	}
+	public void responderComentario(Integer IDVideo, Integer IDCR, String nickU, DtFecha fecha, String contenido){
+		VideoHandler vh=VideoHandler.getInstance();
+		Video v=vh.find(IDVideo);
+		v.responderComentario(IDCR, nickU, fecha, contenido);
+	}
+	public void valorarVideo(Integer IDVideo, String nickU, boolean valoracion){
+		VideoHandler vh=VideoHandler.getInstance();
+		Video v=vh.find(IDVideo);
+		v.valorarVideo(nickU, valoracion);
+	}
+	public DtInfoVideo verDetallesVideoExt(Integer IDVideo){
+		VideoHandler vh=VideoHandler.getInstance();
+		Video v=vh.find(IDVideo);
+		return v.getInfoVideoExt();
+	}
+	public DtVideo infoAddVideo(Integer IDVideo){
+		VideoHandler vh=VideoHandler.getInstance();
+		Video v=vh.find(IDVideo);
+		return v.verDetallesVideo();
+	}
+	public DtVideo[] listarVideos(){
+		VideoHandler vh=VideoHandler.getInstance();
+		return vh.listarVideos();
+	}
+	public DtCategoria[] listarCategorias(){
+		CategoriaHandler ch=CategoriaHandler.getInstance();
+		return ch.listarCategorias();
+	}
+	public void crearCategoria(String nombreCat){
+		CategoriaHandler ch=CategoriaHandler.getInstance();
+		if(!ch.isMember(nombreCat)){
+			Categoria c=new Categoria(nombreCat);
+			ch.addCategoria(c);
+		}
+	}
+	public boolean existeCategoria(String nombreCat){
+		CategoriaHandler ch=CategoriaHandler.getInstance();
+		return ch.isMember(nombreCat);
 	}
 }
