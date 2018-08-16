3230e186f3d951f719d77719ce73391286287ec8
Nacho <Nacho@Ching>
2018-08-16 16:24:10 -0300

    mini fix de IVideoCtrl

--- src/uytube/logica/IVideoCtrl.java
+++ src/uytube/logica/IVideoCtrl.java
@@ -1,16 +1,16 @@
 package uytube.logica;
 
 public interface IVideoCtrl {
-	public DtListaReproduccion [] listarLDRPorCategoria(String cat);
-	public DtVideo[] listarVideosPorCategoria(String cat);
-	public DtComentario[] listarComentarios(Integer IDVideo);
-	public void nuevoComentario(Integer IDVideo,String nickU,DtFecha fecha,String contenido);
-	public void responderComentario(Integer IDVideo, Integer IDCR, String nickU, DtFecha fecha, String contenido);
-	public void valorarVideo(Integer IDVideo, String nickU, boolean valoracion);
-	public DtInfoVideo verDetallesVideoExt(Integer IDVideo);
-	public DtVideo infoAddVideo(Integer IDVideo);
-	public DtVideo[] listarVideos();
-	public DtCategoria[] listarCategorias();
-	public void crearCategoria(String nombreCat);
-	public boolean existeCategoria(String nombreCat);
+	public abstract DtListaReproduccion [] listarLDRPorCategoria(String cat);
+	public abstract DtVideo[] listarVideosPorCategoria(String cat);
+	public abstract DtComentario[] listarComentarios(Integer IDVideo);
+	public abstract void nuevoComentario(Integer IDVideo,String nickU,DtFecha fecha,String contenido);
+	public abstract void responderComentario(Integer IDVideo, Integer IDCR, String nickU, DtFecha fecha, String contenido);
+	public abstract void valorarVideo(Integer IDVideo, String nickU, boolean valoracion);
+	public abstract DtInfoVideo verDetallesVideoExt(Integer IDVideo);
+	public abstract DtVideo infoAddVideo(Integer IDVideo);
+	public abstract DtVideo[] listarVideos();
+	public abstract DtCategoria[] listarCategorias();
+	public abstract void crearCategoria(String nombreCat);
+	public abstract boolean existeCategoria(String nombreCat);
 }
