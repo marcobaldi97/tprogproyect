dea11b3bd63bfb2355bfae055ecb247c1927275d
marco baldi <marco.baldi@fing.edu.uy>
2018-08-17 13:55:38 -0300

    Agregada categoria a DtCanal

--- src/uytube/logica/DtCanal.java
+++ src/uytube/logica/DtCanal.java
@@ -4,11 +4,13 @@ public class DtCanal {
 	private String nombre;
 	private String descripcion;
 	private Boolean privado;
+	private DtCategoria categoria;
 
 	public DtCanal(Canal c){
 		nombre=c.getNombre();
 		descripcion=c.getDescripcion();
 		privado=c.getPrivacidad();
+		categoria = c.getCategoria();
 	}
 	
 	public String getNombre() {
@@ -21,6 +23,10 @@ public class DtCanal {
 	
 	public Boolean getPrivacidad() {
 		return privado;
+	}
+	
+	public DtCategoria getCategoria() {
+		return categoria;
 	}
 	
 	public Boolean equals(DtCanal c) {
