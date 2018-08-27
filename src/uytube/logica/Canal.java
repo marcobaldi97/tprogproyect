b75af7664733de4d83b327d6be08d253da62ddae
Diego Stratta <strattadb@gmail.com>
2018-08-27 17:31:40 -0300

    fix(logica): fix remover LDR en categoria cuando se elimina video

--- src/uytube/logica/Canal.java
+++ src/uytube/logica/Canal.java
@@ -1,4 +1,5 @@
 package uytube.logica;
+
 //prueba commit
 import java.util.HashMap;
 import java.util.Map;
@@ -8,116 +9,120 @@ public class Canal {
 	private String descripcion;
 	private Boolean privado;
 	private Categoria cate;
-	private Map<String,Video> videos;
-	private Map<String,ListaReproduccion> listasReproduccion;
-	
-	public String getNombre(){
+	private Map<String, Video> videos;
+	private Map<String, ListaReproduccion> listasReproduccion;
+
+	public String getNombre() {
 		return nombre;
 	}
-	public String getDescripcion(){
+
+	public String getDescripcion() {
 		return descripcion;
 	}
-	public Boolean getPrivacidad(){
+
+	public Boolean getPrivacidad() {
 		return privado;
 	}
-	public DtCategoria getCategoria(){
-		DtCategoria dt=new DtCategoria(cate);
+
+	public DtCategoria getCategoria() {
+		DtCategoria dt = new DtCategoria(cate);
 		return dt;
 	}
-	
+
 	public void addListaReproduccion(Particular lr) {
 		listasReproduccion.put(lr.getNombre(), lr);
 	}
-	public void addListaReproduccion(PorDefecto lr){
+
+	public void addListaReproduccion(PorDefecto lr) {
 		listasReproduccion.put(lr.getNombre(), lr);
 	}
-	
+
 	public void removeListaReproduccion(ListaReproduccion lr) {
 		listasReproduccion.remove(lr.getNombre());
 	}
-	
+
 	public ListaReproduccion findLista(String s) {
 		return listasReproduccion.get(s);
 	}
-	
+
 	public String[] listarListasReproduccion() {
 		String[] nombresListas = new String[listasReproduccion.size()];
 		Integer contador = 0;
-		for(Map.Entry<String, ListaReproduccion> entry : listasReproduccion.entrySet()) {
-			if(entry.getValue() != null) {
+		for (Map.Entry<String, ListaReproduccion> entry : listasReproduccion.entrySet()) {
+			if (entry.getValue() != null) {
 				String nom = entry.getValue().getNombre();
 				nombresListas[contador] = nom;
 				contador++;
-			}//Se agreg� esta linea para tratar de evitar los punteros nulos.
+			} // Se agreg� esta linea para tratar de evitar los punteros nulos.
 		}
 		return nombresListas;
 	}
-	
+
 	public DtListaReproduccion verDetallesListareproduccion(String nombreLista) {
-		DtListaReproduccion dt=null;
-		if(memberListaReproduccionPropia(nombreLista)){
+		DtListaReproduccion dt = null;
+		if (memberListaReproduccionPropia(nombreLista)) {
 			ListaReproduccion lr = findLista(nombreLista);
 			dt = lr.verDetallesListareproduccion();
 		}
 		return dt;
 	}
-	
-	public Canal(String nom,String pro, String desc, Boolean privacidadE,String catE) {
+
+	public Canal(String nom, String pro, String desc, Boolean privacidadE, String catE) {
 		nombre = nom;
 		descripcion = desc;
 		privado = privacidadE;
-		if(catE!=null){
+		if (catE != null) {
 			CategoriaHandler ch = CategoriaHandler.getInstance();
 			cate = ch.find(catE);
-		}else{
-			SystemHandler sh=SystemHandler.getInstance();
+		} else {
+			SystemHandler sh = SystemHandler.getInstance();
 			cate = sh.getSinCat();
 		}
-		videos = new HashMap<String,Video>();
-		listasReproduccion = new HashMap<String,ListaReproduccion>();
+		videos = new HashMap<String, Video>();
+		listasReproduccion = new HashMap<String, ListaReproduccion>();
 		SystemHandler sh = SystemHandler.getInstance();
 		DtListaReproduccion[] listasDefault = sh.obtenerListasReproduccion();
-		for(int index = 0;index<listasDefault.length;index++){
-			PorDefecto lr = new PorDefecto(listasDefault[index].getNombre(),pro);
+		for (int index = 0; index < listasDefault.length; index++) {
+			PorDefecto lr = new PorDefecto(listasDefault[index].getNombre(), pro);
 			addListaReproduccion(lr);
 		}
 	}
-	
+
 	public DtCanal mostrarInfoCanal() {
 		DtCanal dt = new DtCanal(this);
 		return dt;
 	}
-	
+
 	public void addVideo(Video v) {
 		videos.put(v.getNombre(), v);
 	}
-	
+
 	public void removerVideo(Video v) {
 		videos.remove(v.getNombre());
 	}
-	
+
 	public Video findVideo(String s) {
 		return videos.get(s);
 	}
-	
-	public void aniadirVideo(String nom, String pro, String desc, Integer dur, DtFecha fp, String url, DtCategoria catE, boolean p) {
+
+	public void aniadirVideo(String nom, String pro, String desc, Integer dur, DtFecha fp, String url, DtCategoria catE,
+			boolean p) {
 		Video v = new Video(nom, pro, desc, dur, fp, url, catE, p);
 		this.addVideo(v);
 		VideoHandler vidH = VideoHandler.getInstance();
 		vidH.addVideo(v);
 	}
-	
-	public void ingresarNuevosDatosVideo(String nom, String d, int dur, DtFecha fp, String url, DtCategoria catE, boolean p) {
+
+	public void ingresarNuevosDatosVideo(String nom, String d, int dur, DtFecha fp, String url, DtCategoria catE,
+			boolean p) {
 		Video v = videos.get(nom);
 		v.ingresarNuevosDatosVideo(d, dur, fp, url, catE, p);
 	}
-	
-	
-	
+
 	public String[] listarVideosCanal() {
 		String[] nombresVideos = new String[videos.size()];
 		Integer contador = 0;
-		for(Map.Entry<String, Video> entry : videos.entrySet()) {
+		for (Map.Entry<String, Video> entry : videos.entrySet()) {
 			String nom = entry.getValue().getNombre();
 			nombresVideos[contador] = nom;
 			contador++;
@@ -126,7 +131,7 @@ public class Canal {
 	}
 
 	public String[] listarVideosPorLDR(String nombreLDR) {
-		
+
 		return listasReproduccion.get(nombreLDR).listarVideos();
 	}
 
@@ -135,39 +140,41 @@ public class Canal {
 		Video v = vh.find(id);
 		listasReproduccion.get(nombreLDR).agregarVideo(v);
 	}
-	
+
 	public void cambiarPrivLDR(String nombreL, Boolean privE) {
-		Particular lr =  (Particular) listasReproduccion.get(nombreL);
+		Particular lr = (Particular) listasReproduccion.get(nombreL);
 		lr.cambiarPrivLDR(privE);
 	}
-	
+
 	public void eliminarVideoLista(Integer id_video, String nombreLDR) {
 		// TODO Auto-generated method stub
-		ListaReproduccion lr=listasReproduccion.get(nombreLDR);
+		ListaReproduccion lr = listasReproduccion.get(nombreLDR);
 		lr.removerVideo(id_video);
 	}
-	
+
 	public Boolean memberListaReproduccionPropia(String nombreLista) {
 		return listasReproduccion.containsKey(nombreLista);
 	}
-	
-	public void nuevaListaPorDefecto(String nombreL,String pro) {
-		PorDefecto ldr=new PorDefecto(nombreL, pro);
+
+	public void nuevaListaPorDefecto(String nombreL, String pro) {
+		PorDefecto ldr = new PorDefecto(nombreL, pro);
 		listasReproduccion.put(nombreL, ldr);
 	}
-	
+
 	public DtVideo obtenerInfoAdicVideo(String nombreVideo) {
 		DtVideo dt = new DtVideo(videos.get(nombreVideo));
 		return dt;
 	}
-	
+
 	public Boolean memberVideoEnUsuario(String nombreVideo) {
 		return videos.containsKey(nombreVideo);
 	}
+
 	public String[] listarVideosListaReproduccionUsuario(String nombreLista) {
 		ListaReproduccion lr = listasReproduccion.get(nombreLista);
 		return lr.listarVideos();
 	}
+
 	public DtVideo[] obtenerDtsVideosListaReproduccionUsuario(String nombreLista) {
 		// TODO Auto-generated method stub
 		ListaReproduccion lr = listasReproduccion.get(nombreLista);
--- src/uytube/logica/ListaReproduccion.java
+++ src/uytube/logica/ListaReproduccion.java
@@ -1,67 +1,72 @@
 package uytube.logica;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class ListaReproduccion {
 	private String nombre;
 	private String propietario;
-	private Map<Integer,Video> videos;
-	private Map<String,Categoria> categorias;
-	
+	private Map<Integer, Video> videos;
+	private Map<String, Categoria> categorias;
+
 	public ListaReproduccion(String nombLDR, String pro) {
-		nombre=nombLDR;
-		propietario=pro;
-		videos = new HashMap<Integer,Video>();
-		categorias = new HashMap<String,Categoria>();
+		nombre = nombLDR;
+		propietario = pro;
+		videos = new HashMap<Integer, Video>();
+		categorias = new HashMap<String, Categoria>();
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public abstract DtListaReproduccion verDetallesListareproduccion();
-	
-	public String[] listarVideos(){
+
+	public String[] listarVideos() {
 		String[] nombreVideos = new String[videos.size()];
 		Integer contador = 0;
-		for(Map.Entry<Integer, Video> entry : videos.entrySet()) {
+		for (Map.Entry<Integer, Video> entry : videos.entrySet()) {
 			nombreVideos[contador] = entry.getValue().getNombre();
 			contador++;
 		}
 		return nombreVideos;
 	}
-	
-	private void refrescarCategorias(){
-		for(Map.Entry<Integer, Video> entry : videos.entrySet()) {
-			categorias.put(entry.getValue().getObjetoCategoria().getNombre(), entry.getValue().getObjetoCategoria());
+
+	private void refrescarCategorias() {
+		for (Map.Entry<Integer, Video> entry : videos.entrySet()) {
+			Categoria categoria = entry.getValue().getObjetoCategoria();
+			categoria.aniadirLDR(this);
+
+			categorias.put(categoria.getNombre(), entry.getValue().getObjetoCategoria());
 		}
-		
+
 	}
-	
+
 	public void removerVideo(Integer id) {
-		String categoriaNombre = videos.get(id).getCategoria().getNombre();
-		categorias.remove(categoriaNombre);		
+		Categoria categoria = videos.get(id).getObjetoCategoria();
+		categorias.remove(categoria.getNombre());
+		categoria.removerLDR(this);
 		videos.remove(id);
 		refrescarCategorias();
 	}
 
 	public void agregarVideo(Video v) {
-		
-			videos.put(v.getIDVideo(), v);
-			refrescarCategorias();
-			
-			CategoriaHandler catH = CategoriaHandler.getInstance();
-			Categoria c = catH.find(v.getCategoria().getNombre());
-			if(c!=null)
-				c.aniadirLDR(this);
-		
+
+		videos.put(v.getIDVideo(), v);
+		refrescarCategorias();
+
+		CategoriaHandler catH = CategoriaHandler.getInstance();
+		Categoria c = catH.find(v.getCategoria().getNombre());
+		if (c != null)
+			c.aniadirLDR(this);
+
 	};
-	
-	public DtCategoria[] getInfoCategorias(){
-		DtCategoria[] res=new DtCategoria[categorias.size()];
-		int i=0;
-		for(Map.Entry<String,Categoria> entry:categorias.entrySet()){
-			res[i]=new DtCategoria(entry.getValue());
+
+	public DtCategoria[] getInfoCategorias() {
+		DtCategoria[] res = new DtCategoria[categorias.size()];
+		int i = 0;
+		for (Map.Entry<String, Categoria> entry : categorias.entrySet()) {
+			res[i] = new DtCategoria(entry.getValue());
 			i++;
 		}
 		return res;
@@ -78,7 +83,7 @@ public abstract class ListaReproduccion {
 	public DtVideo[] obtenerDtsVideosListaReproduccionUsuario(String nombreLista) {
 		DtVideo[] dtvideos = new DtVideo[videos.size()];
 		int contador = 0;
-		for(Map.Entry<Integer, Video> entry : videos.entrySet()) {
+		for (Map.Entry<Integer, Video> entry : videos.entrySet()) {
 			DtVideo dt = new DtVideo(entry.getValue());
 			dtvideos[contador] = dt;
 			contador++;
