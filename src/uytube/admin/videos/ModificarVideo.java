0d199e33b8de3a33fccae3c4de249a16ac23288b
Diego Stratta <strattadb@gmail.com>
2018-08-28 21:38:54 -0300

    feat(admin): finish modificar video GUI

--- src/uytube/admin/videos/ModificarVideo.java
+++ src/uytube/admin/videos/ModificarVideo.java
@@ -27,65 +27,68 @@ import uytube.logica.IUsuarioCtrl;
 import uytube.logica.IVideoCtrl;
 
 public class ModificarVideo extends JInternalFrame {
-	private JComboBox comboBoxNombreVideo;
-	private JTextField textFieldURL;
-	private JDateChooser dateChooserFecha;
-	private JTextArea textAreaDesc;
-	private JComboBox comboBoxCategoria;
-	private JSpinner spinnerDuracion;
-	private JComboBox comboBoxPrivacidad;
-	
-    private static void infoBox(String infoMessage, String titleBar){
-        JOptionPane.showMessageDialog(null, infoMessage, "" + titleBar, JOptionPane.INFORMATION_MESSAGE);
-    }
-    
-    private void cargarDatos(DtVideo datosVideo) {
+	private JComboBox comboBoxNombreVideo = new JComboBox();
+	private JTextField textFieldURL = new JTextField();
+	private JDateChooser dateChooserFecha = new JDateChooser();
+	private JTextArea textAreaDesc = new JTextArea();
+	private JComboBox comboBoxCategoria = new JComboBox();
+	private JSpinner spinnerDuracion = new JSpinner();
+	private JComboBox comboBoxPrivacidad = new JComboBox();
+
+	private static void infoBox(String infoMessage, String titleBar) {
+		JOptionPane.showMessageDialog(null, infoMessage, "" + titleBar, JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	private void cargarDatos(DtVideo datosVideo) {
 		textFieldURL.setText(datosVideo.getUrl());
 		textAreaDesc.setText(datosVideo.getDescripcion());
 		comboBoxCategoria.setSelectedItem(datosVideo.getCategoria().getNombre());
 		spinnerDuracion.setValue(datosVideo.getDuracion());
-		if(datosVideo.getPrivacidad() == true) comboBoxPrivacidad.setSelectedItem("Privado");
-		else comboBoxPrivacidad.setSelectedItem("Publico");
+		if (datosVideo.getPrivacidad() == true)
+			comboBoxPrivacidad.setSelectedItem("Privado");
+		else
+			comboBoxPrivacidad.setSelectedItem("Publico");
 		dateChooserFecha.setDate(datosVideo.getFechaPublicacion().getFecha());
-    }
-    
-    private void limpiarDatos() {
-    	textFieldURL.setText("");
-    	Date dateRespawn = new Date();
-    	dateChooserFecha.setDate(dateRespawn);
-    	textAreaDesc.setText("");
-    	comboBoxCategoria.setSelectedItem("");
-    	spinnerDuracion.setValue(0);
-    	comboBoxPrivacidad.setSelectedItem("Privado");
-    }
-    
-    private void bloquearDatos() {
-    	textFieldURL.setEnabled(false);
-    	textAreaDesc.setEnabled(false);
-    	comboBoxCategoria.setEnabled(false);
-    	spinnerDuracion.setEnabled(false);
-    	comboBoxPrivacidad.setEnabled(false);
-    }
-    
-    private void habilitarDatos() {
-    	textFieldURL.setEnabled(true);
-    	textAreaDesc.setEnabled(true);
-    	comboBoxCategoria.setEnabled(true);
-    	spinnerDuracion.setEnabled(true);
-    	comboBoxPrivacidad.setEnabled(true);
-    }
-	
+	}
+
+	private void limpiarDatos() {
+		textFieldURL.setText("");
+		Date dateRespawn = new Date();
+		dateChooserFecha.setDate(dateRespawn);
+		textAreaDesc.setText("");
+		comboBoxCategoria.setSelectedItem("");
+		spinnerDuracion.setValue(0);
+		comboBoxPrivacidad.setSelectedItem("Privado");
+	}
+
+	private void bloquearDatos() {
+		textFieldURL.setEnabled(false);
+		textAreaDesc.setEnabled(false);
+		comboBoxCategoria.setEnabled(false);
+		spinnerDuracion.setEnabled(false);
+		comboBoxPrivacidad.setEnabled(false);
+	}
+
+	private void habilitarDatos() {
+		textFieldURL.setEnabled(true);
+		textAreaDesc.setEnabled(true);
+		comboBoxCategoria.setEnabled(true);
+		spinnerDuracion.setEnabled(true);
+		comboBoxPrivacidad.setEnabled(true);
+	}
+
 	/**
 	 * Create the frame.
 	 */
 	public ModificarVideo(IUsuarioCtrl iCU, IVideoCtrl iCV) {
 		setTitle("Modificar Video");
 		setMaximizable(true);
+		setResizable(true);
 		setIconifiable(true);
 		setClosable(true);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new GridLayout(9, 2, 5, 5));
-		
+
 		JLabel lblNicknameAutor = new JLabel("Nickname Autor:");
 		lblNicknameAutor.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNicknameAutor.setVerticalAlignment(SwingConstants.CENTER);
@@ -93,118 +96,111 @@ public class ModificarVideo extends JInternalFrame {
 		JComboBox comboBoxNicknames = new JComboBox();
 		comboBoxNicknames.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(comboBoxNicknames.getSelectedIndex() != -1) {
-					if(comboBoxNicknames.getSelectedItem().equals("") != true) {
-						comboBoxNombreVideo.setEnabled(true);
-						String[] nombresVideos = iCU.listarVideosCanal(comboBoxNicknames.getSelectedItem().toString());
-						comboBoxNombreVideo.setModel(new DefaultComboBoxModel(nombresVideos));
-					}else {
-						comboBoxNombreVideo.setEnabled(false);
+				comboBoxNombreVideo.removeAllItems();
+				if (comboBoxNicknames.getSelectedIndex() != -1) {
+					String[] nombresVideos = iCU.listarVideosCanal(comboBoxNicknames.getSelectedItem().toString());
+
+					for (final String nombreVideo : nombresVideos) {
+						comboBoxNombreVideo.addItem(nombreVideo);
 					}
 				}
 			}
 		});
-		comboBoxNicknames.setEditable(true);
 		String[] nicknamesArray = iCU.listarNicknamesUsuarios();
-		comboBoxNicknames.setModel(new DefaultComboBoxModel(nicknamesArray));
+		for (final String nickname : nicknamesArray) {
+			comboBoxNicknames.addItem(nickname);
+		}
+		comboBoxNicknames.setSelectedIndex(-1);
 		getContentPane().add(comboBoxNicknames);
-		
+
 		JLabel lblNombreVideo = new JLabel("Nombre Video:");
 		lblNombreVideo.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(lblNombreVideo);
-		
-		comboBoxNombreVideo = new JComboBox();
-		comboBoxNombreVideo.setEnabled(false);
+
 		comboBoxNombreVideo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				if(comboBoxNicknames.getSelectedIndex() != -1) {
-					if(comboBoxNombreVideo.getSelectedItem().equals("") != true) {
-						String nickname = comboBoxNicknames.getSelectedItem().toString();
-						String nombreVideo = comboBoxNombreVideo.getSelectedItem().toString();
-						DtVideo datosVideo = iCU.obtenerInfoAdicVideo(nickname, nombreVideo);
-						textFieldURL.setText(datosVideo.getUrl());
-						String textAreaPopulator = datosVideo.getDescripcion();
-						textAreaDesc.setText(textAreaPopulator);
-						comboBoxCategoria.setSelectedItem(datosVideo.getCategoria().getNombre());
-						spinnerDuracion.setValue(datosVideo.getDuracion());
-						if(datosVideo.getPrivacidad() == true) comboBoxPrivacidad.setSelectedItem("Privado");
-						else comboBoxPrivacidad.setSelectedItem("Publico");
-						dateChooserFecha.setDate(datosVideo.getFechaPublicacion().getFecha());
-					}else {
-						limpiarDatos();
-					}
+				if (comboBoxNicknames.getSelectedIndex() != -1) {
+					String nickname = comboBoxNicknames.getSelectedItem().toString();
+					String nombreVideo = comboBoxNombreVideo.getSelectedItem().toString();
+					DtVideo datosVideo = iCU.obtenerInfoAdicVideo(nickname, nombreVideo);
+					cargarDatos(datosVideo);
 				}
 			}
 		});
 		getContentPane().add(comboBoxNombreVideo);
-		
+
 		JLabel lblNewLabel = new JLabel("URL Video:");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(lblNewLabel);
-		
-		textFieldURL = new JTextField();
+
 		getContentPane().add(textFieldURL);
 		textFieldURL.setColumns(10);
-		
+
 		JLabel lblDescripcion = new JLabel("Descripcion:");
 		lblDescripcion.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(lblDescripcion);
-		
-		JTextArea textAreaDesc = new JTextArea();
+
 		getContentPane().add(textAreaDesc);
-		
+
 		JLabel lblDuracion = new JLabel("Duracion:");
 		lblDuracion.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(lblDuracion);
-		
-		spinnerDuracion = new JSpinner();
+
 		spinnerDuracion.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), null, new Integer(1)));
 		getContentPane().add(spinnerDuracion);
-		
+
 		JLabel lblFecha = new JLabel("Fecha:");
 		lblFecha.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(lblFecha);
-		
-		dateChooserFecha = new JDateChooser();
+
 		getContentPane().add(dateChooserFecha);
-		
+
 		JLabel lblCategoria = new JLabel("Categoria:");
 		lblCategoria.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(lblCategoria);
-		
-		
-		comboBoxCategoria = new JComboBox();
-		//empiezo a cargar las categorias.
+
+		// empiezo a cargar las categorias.
 		DtCategoria[] categoriasDts = iCV.listarCategorias();
-		String[] nombresCategoriasArray = new String[categoriasDts.length];
-		for(int i = 0; i < categoriasDts.length; i++) {
-			nombresCategoriasArray[i] = categoriasDts[i].getNombre();
+		for (final DtCategoria categoria : categoriasDts) {
+			comboBoxCategoria.addItem(categoria.getNombre());
 		}
-		comboBoxCategoria.setModel(new DefaultComboBoxModel(nombresCategoriasArray));
-		//termino de cargar las categorias
+		comboBoxCategoria.setSelectedIndex(-1);
+		// termino de cargar las categorias
 		getContentPane().add(comboBoxCategoria);
-		
+
 		JLabel lblPrivacidad = new JLabel("Privacidad:");
 		lblPrivacidad.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(lblPrivacidad);
-		
-		comboBoxPrivacidad = new JComboBox();
-		comboBoxPrivacidad.setModel(new DefaultComboBoxModel(new String[] {"Privado", "Publico"}));
+
+		comboBoxPrivacidad.setModel(new DefaultComboBoxModel(new String[] { "Privado", "Publico" }));
+		comboBoxPrivacidad.setSelectedIndex(-1);
 		getContentPane().add(comboBoxPrivacidad);
-		
+
 		JButton btnNewButtonCancelar = new JButton("Cancelar");
 		btnNewButtonCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
 				dispose();
 			}
 		});
 		getContentPane().add(btnNewButtonCancelar);
-		
+
 		JButton btnNewButtonAceptar = new JButton("Aceptar");
 		btnNewButtonAceptar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				final String nickname = (String) comboBoxNicknames.getSelectedItem();
+				final String nombreVideo = (String) comboBoxNombreVideo.getSelectedItem();
+				final String descripcion = textAreaDesc.getText();
+				final int duracion = (int) spinnerDuracion.getValue();
+				final DtFecha fecha = new DtFecha(dateChooserFecha.getDate());
+				final String url = textFieldURL.getText();
+				final DtCategoria categoria = new DtCategoria((String) comboBoxCategoria.getSelectedItem());
+				final boolean privado = ((String) comboBoxPrivacidad.getSelectedItem()).equals("Privado");
 
+				iCU.ingresarNuevosDatosVideo(nickname, nombreVideo, descripcion, duracion, fecha, url, categoria,
+						privado);
+
+				infoBox("Video modificado con exito", "Modificar video");
+				dispose();
 			}
 		});
 		getContentPane().add(btnNewButtonAceptar);
--- src/uytube/logica/DtCategoria.java
+++ src/uytube/logica/DtCategoria.java
@@ -1,21 +1,24 @@
 package uytube.logica;
 
-
 public class DtCategoria {
 	private String nombre;
-	
-	public DtCategoria(Categoria c){
-		nombre=c.getNombre();
+
+	public DtCategoria(Categoria c) {
+		nombre = c.getNombre();
 	}
-	
+
+	public DtCategoria(final String nombre) {
+		this.nombre = nombre;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public boolean equals(DtCategoria dt) {
-		if(dt!=null){
+		if (dt != null) {
 			return nombre == dt.getNombre();
-		}else
+		} else
 			return false;
 	}
 
