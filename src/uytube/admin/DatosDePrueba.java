db3e7180e7a643b623857038b6c9cea2b4459ca4
maria <maria.usuca@fing.edu.uy>
2018-08-30 19:32:31 -0300

    faltan comentarios cargar

--- src/uytube/admin/DatosDePrueba.java
+++ src/uytube/admin/DatosDePrueba.java
@@ -145,21 +145,26 @@ public class DatosDePrueba {
 		//VIDEOS
 		//public void aniadirVideo(String nickU, String nom, String desc, Integer dur, DtFecha fp, String url,
 			//	DtCategoria catE, boolean p) {
-		String V1="Locura celeste";
 		DtCategoria catMUS = new DtCategoria(MUS);
 		DtCategoria catNOT = new DtCategoria(NOT);
 		DtCategoria catCAR = new DtCategoria(CAR);
 		DtCategoria catDEP = new DtCategoria(DEP);
 		DtCategoria catCYT = new DtCategoria(CYT);
+		
+		String V1="Locura celeste";
 		ICU.aniadirVideo(TC, V1, null, duracion, fechaNac,"https://youtu.be/PAfbzKcePx0",catMUS, false);
+		ICU.aniadirVideo(CS, V1, null, duracion, fechaNac,"https://youtu.be/PAfbzKcePx0",catMUS, false);
 		String V2="Niño payaso";
-		ICU.aniadirVideo(TS, V2, null, duracion, fechaNac,"https://youtu.be/K-uEIUnyZPg",catMUS, false);
+		ICU.aniadirVideo(TC, V2, null, duracion, fechaNac,"https://youtu.be/K-uEIUnyZPg",catMUS, false);
+		ICU.aniadirVideo(CS, V2, null, duracion, fechaNac,"https://youtu.be/K-uEIUnyZPg",catMUS, false);
 		String V3="Sweet child'o mine";
 		ICU.aniadirVideo(JB, V3, null, duracion, fechaNac,"https://youtu.be/1w7OgIMMRc4",catMUS,true);
+		ICU.aniadirVideo(KH, V3, null, duracion, fechaNac,"https://youtu.be/1w7OgIMMRc4",catMUS,true);
 		String V4="Dancing in the Dark";
 		ICU.aniadirVideo(KH, V4, null, duracion, fechaNac,"https://youtu.be/129kuDCQtHs", catMUS, true);
 		String V5="Thriler";
 		ICU.aniadirVideo(JB, V5, null, duracion, fechaNac,"https://youtu.be/sOnqjkJTMaA",catMUS,true);
+		ICU.aniadirVideo(KH, V5, null, duracion, fechaNac,"https://youtu.be/sOnqjkJTMaA",catMUS,true);
 		String V6="100 años de FING";
 		ICU.aniadirVideo(HG, V6, null, duracion, fechaNac,"https://youtu.be/peGS4TBxSaI",catNOT ,true);
 		String V7="50 años del InCo";
@@ -174,20 +179,108 @@ public class DatosDePrueba {
 		ICU.aniadirVideo(JB, V11, null, duracion, fechaNac,"https://youtu.be/g46w4_kD_lA", catDEP, true);
 		String V12="Pacheco goles mas recordados";
 		ICU.aniadirVideo(TC, V12, null, duracion, fechaNac,"https://youtu.be/wlEd6-HsIxI", catDEP, false);
+		ICU.aniadirVideo(AP, V12, null, duracion, fechaNac,"https://youtu.be/wlEd6-HsIxI", catDEP, false);
 		String V13="Inaguracion Estadio Peñarol";
 		ICU.aniadirVideo(JB, V13, null, duracion, fechaNac,"https://youtu.be/U6XPJ8Vz72A",catDEP ,true);
 		String V14="Recoba 20 mejores goles";
 		ICU.aniadirVideo(CS, V14, null, duracion, fechaNac,"https://youtu.be/Gy3fZhWdLEQ", catDEP, false);
+		ICU.aniadirVideo(AR, V14, null, duracion, fechaNac,"https://youtu.be/Gy3fZhWdLEQ", catDEP, false);
 		String V15="Entrevita a director CUTI";
 		ICU.aniadirVideo(NJ, V15, null, duracion, fechaNac,"https://youtu.be/Eq5uBEzI6qs",catCYT,true);
 		String V16="Ventana al futuro Uruguay y deficit de ingenieros";
 		ICU.aniadirVideo(NJ, V16, null, duracion, fechaNac,"https://youtu.be/zBR2pnASlQE",catCYT ,true);
 		
+		//LISTAS PARTICULARES //tienen categoria??
+		//ICU.nuevaListaParticular(String nickU, String nombreL, Boolean privada)
+		String LP1="Nostalgia";	ICU.nuevaListaParticular(KH,LP1,true); //MUS LP1
+		String LP2="De fiesta"; ICU.nuevaListaParticular(TC,LP2,false);//MUS DEP LP2
+ 		String LP3="Novedades FING"; ICU.nuevaListaParticular(HG,LP3,true);//NOT LP3
+		String LP4="De todo un poco";ICU.nuevaListaParticular(CS,LP4,false);//MUS DEP CAR LP4
+		String LP5="Noticias y CYT";ICU.nuevaListaParticular(NJ,LP5,true); //NOT CYT LP5
+		String LP6="Solo deportes";ICU.nuevaListaParticular(JB,LP6,true); //dep LP6
 		
-		ICV.valorarVideo(1, "RobertoDeportista", false);
+		//DtVideo video = ICU.obtenerInfoAdicVideo(nicknameAutor, nombreVideo);
+	//	int idVideo = video.getIDVideo();
+		//ICU.agregarVideoLista(nicknameUsuario, idVideo, nombreListaReproduccion);
+		DtVideo video = ICU.obtenerInfoAdicVideo(KH,V3);
+		int idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(KH,idVideo,LP1);
+		
+		video = ICU.obtenerInfoAdicVideo(KH,V4);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(KH,idVideo,LP1);
+		
+		video = ICU.obtenerInfoAdicVideo(JB,V5);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(KH,idVideo,LP1);
+		
+		video = ICU.obtenerInfoAdicVideo(TC,V1);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(TC,idVideo,LP2);
+		
+		video = ICU.obtenerInfoAdicVideo(TC,V2);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(TC,idVideo,LP2);
+		
+		video = ICU.obtenerInfoAdicVideo(CS,V10);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(TC,idVideo,LP2);
+		
+		video = ICU.obtenerInfoAdicVideo(HG,V6);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(HG,idVideo,LP3);
+		
+		video = ICU.obtenerInfoAdicVideo(HG,V7);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(HG,idVideo,LP3);
+		
+		video = ICU.obtenerInfoAdicVideo(HG,V8);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(HG,idVideo,LP3);
+		
+		video = ICU.obtenerInfoAdicVideo(CS,V1);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(CS,idVideo,LP4);
+		
+		video = ICU.obtenerInfoAdicVideo(CS,V2);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(CS,idVideo,LP4);
+		
+		video = ICU.obtenerInfoAdicVideo(CS,V9);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(CS,idVideo,LP4);
+		
+		video = ICU.obtenerInfoAdicVideo(CS,V10);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(CS,idVideo,LP4);
+		
+		video = ICU.obtenerInfoAdicVideo(JB,V13);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(CS,idVideo,LP4);
+		
+		video = ICU.obtenerInfoAdicVideo(HG,V8);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(NJ,idVideo,LP5);
+		video = ICU.obtenerInfoAdicVideo(NJ,V16);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(NJ,idVideo,LP5);
+
+		video = ICU.obtenerInfoAdicVideo(JB,V11);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(JB,idVideo,LP6);
+
+		video = ICU.obtenerInfoAdicVideo(JB,V13);
+		idVideo = video.getIDVideo();
+		ICU.agregarVideoLista(JB,idVideo,LP6);
+
+		//LISTAS POR DEFECTO
+		ICU.nuevaListaPorDefecto("Escuchar mas tarde"); //LD1
+		ICU.nuevaListaPorDefecto("Deporte total"); //LD2
+		ICU.nuevaListaPorDefecto("Novedades generales");//LD3
 
 		VideoHandler vh=VideoHandler.getInstance();
 		DtVideo dtVideo =vh.member(V7);
+	
 		fecha.setDate(5);fecha.setMonth(12);fecha.setYear(17);fecha.setTime(1435);
 		DtFecha fechaVideo = new DtFecha(fecha); //5,12,2017,14:35
 		ICV.nuevoComentario(dtVideo.getIDVideo(),NJ, fechaVideo,"Fue un gran evento" );
@@ -228,7 +321,33 @@ public class DatosDePrueba {
 		fechaVideo = new DtFecha(fecha);
 		ICV.nuevoComentario(dtVideo.getIDVideo(), , fechaVideo, "");
 		
+		//ME GUSTA
+		video = ICU.obtenerInfoAdicVideo(HG,V7);
+		ICV.valorarVideo(video.getIDVideo(),SP, false);
 		
+		video = ICU.obtenerInfoAdicVideo(HG,V8);
+		ICV.valorarVideo(video.getIDVideo(),SP, true);
+		
+		video = ICU.obtenerInfoAdicVideo(JB,V11);
+		ICV.valorarVideo(video.getIDVideo(),SP, true);
+		
+		video = ICU.obtenerInfoAdicVideo(CS,V1);
+		ICV.valorarVideo(video.getIDVideo(),NJ, false);
+		
+		video = ICU.obtenerInfoAdicVideo(HG,V7);
+		ICV.valorarVideo(video.getIDVideo(),NJ, true);
+		
+		video = ICU.obtenerInfoAdicVideo(HG,V7);
+		ICV.valorarVideo(video.getIDVideo(),KH, true);
+		
+		video = ICU.obtenerInfoAdicVideo(JB,V13);
+		ICV.valorarVideo(video.getIDVideo(),KH, true);
+		
+		video = ICU.obtenerInfoAdicVideo(CS,V1);
+		ICV.valorarVideo(video.getIDVideo(),MT, true);
+		
+		video = ICU.obtenerInfoAdicVideo(KH,V4);
+		ICV.valorarVideo(video.getIDVideo(),MT, true);
 		
 		
 		ICV.responderComentario(1, 2, "BokuNoNaruto", fechaNacRoberto, "Noo");
