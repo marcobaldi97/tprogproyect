76f954ff655253c51ddaa70735cad38abbfb7787
maria <maria.usuca@fing.edu.uy>
2018-09-21 17:56:18 -0300

    Crea video

--- WEB/src/uytubeWeb/servlets/VideoServlet.java
+++ WEB/src/uytubeWeb/servlets/VideoServlet.java
@@ -1,6 +1,9 @@
 package uytubeWeb.servlets;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -8,6 +11,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import uytube.logica.DtCategoria;
+import uytube.logica.DtFecha;
+import uytube.logica.Factory;
+import uytube.logica.IUsuarioCtrl;
+import uytube.logica.IVideoCtrl;
+import uytube.logica.Usuario;
+import uytube.logica.SystemHandler.Privacidad;
+
 
 /**
  * Servlet implementation class VideoServlet
@@ -24,9 +36,42 @@ public class VideoServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
     
-    private void crearVideo(String nomVideo, String duracion, String url, String fecha, String categoria){
-    	System.out.println(fecha);
-	 }
+    private void creaUsrPrueba(){
+    	   Factory fabrica = Factory.getInstance();
+     	   IUsuarioCtrl usrCtrl = fabrica.getIUsuarioCtrl();
+		Date fecha = ParseFecha("25/02/1962");
+		DtFecha fechaNac = new DtFecha(fecha);
+		String HR="hrubio";
+		usrCtrl.nuevoUsuario("horacio", "Horacio", "Rubino", "horacio.rubino@guambia.com.uy", fechaNac, null,
+				"Canal Horacio", "El canal Horacio es para publicar contenido divertido",Privacidad.PUBLICO, "Entretenimiento");
+    }
+    public static Date ParseFecha(String fecha)
+    {
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date fechaDate = null;
+        try {
+            fechaDate = formato.parse(fecha);
+        } 
+        catch (ParseException ex) 
+        {
+            System.out.println(ex);
+        }
+        return fechaDate;
+    }
+    private void crearVideo(String nomVideo, String duracion, String url, String fecha, String categoria, String descripcionV ){
+       Factory fabrica = Factory.getInstance();
+ 	   IUsuarioCtrl usrCtrl = fabrica.getIUsuarioCtrl();
+ 	   IVideoCtrl ICV = fabrica.getIVideoCtrl();
+ 	   creaUsrPrueba();
+ 	   ICV.crearCategoria(categoria); //crea la categoria para que no haya problemas
+ 	   DtFecha fechaPublicacionV= new DtFecha(ParseFecha(fecha));
+ 	   DtCategoria catV = new DtCategoria(categoria);
+ 	   usrCtrl.aniadirVideo("horacio", nomVideo, descripcionV,(Integer.parseInt(duracion)), fechaPublicacionV, url, catV, Privacidad.PUBLICO);
+ 	   Boolean ok=  usrCtrl.memberVideoEnUsuario("horacio",nomVideo);
+ 	   if(ok){
+ 		   System.out.println("OKKKK");
+ 	   }
+    }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -39,7 +84,7 @@ public class VideoServlet extends HttpServlet {
 		case "altaVideo":
 			System.out.println("Quiero crear video");
 			crearVideo(request.getParameter("nombreVideo"),request.getParameter("duracionVideo"),request.getParameter("urlVideo"),
-					request.getParameter("fechaVideo"),request.getParameter("categoria"));
+					request.getParameter("fechaVideo"),request.getParameter("categoria"),request.getParameter("descVideo"));
 		break;
 		case "null":
 		break;
