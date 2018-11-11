64d5ebe179d80fcbde139df4422f7a886651a0ef
Maria Jose Usuca Ojeda <maria.usuca@fing.edu.uy>
2018-11-11 20:42:26 -0300

    muestra datos usr eliminado

--- GUI/src/uytube/admin/usuarios/verUsuariosEliminados.java
+++ GUI/src/uytube/admin/usuarios/verUsuariosEliminados.java
@@ -11,8 +11,11 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListSelectionModel;
 
+import uyTubePersistencia.Canal;
+import uyTubePersistencia.ListaReproduccion;
 import uyTubePersistencia.PersistenciaCtrl;
 import uyTubePersistencia.Usuario;
+import uyTubePersistencia.Video;
 import uytubeLogic.logica.Puntuacion;
 
 import javax.swing.JTable;
@@ -20,11 +23,16 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.border.TitledBorder;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.GridLayout;
 
 public class verUsuariosEliminados extends JInternalFrame {
 	private JTable tableCanal;
 	private JTable tableUsuarios;
 	private JTable tableDatosUsr;
+	private JLabel lblDatosCanal;
+	private JList listListas;
+	private JList listVideos;
+	private JLabel lblListas;
 
 	/**
 	 * Launch the application.
@@ -45,6 +53,13 @@ public class verUsuariosEliminados extends JInternalFrame {
 	/**
 	 * Create the frame.
 	 */
+	 public void limpiarTabla(JTable tabla){
+        DefaultTableModel tb = (DefaultTableModel) tabla.getModel();
+        int a = tabla.getRowCount()-1;
+        for (int i = a; i>= 0; i--) {          
+        	tb.removeRow(tb.getRowCount()-1);
+        }
+    }
 	DefaultTableModel ModeloUsuario() 
 	{
 		DefaultTableModel model = new DefaultTableModel(); 
@@ -62,6 +77,15 @@ public class verUsuariosEliminados extends JInternalFrame {
 		model.addColumn("Fecha Nacimiento");
 		return model;
 	};
+	DefaultTableModel ModeloCanal()
+	{
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("ID");
+		model.addColumn("Nombre");
+		model.addColumn("Descripcion");
+		model.addColumn("Privacidad");
+		return model;
+	};
 	
 	public verUsuariosEliminados() {
 		setClosable(true);
@@ -69,7 +93,7 @@ public class verUsuariosEliminados extends JInternalFrame {
 		setIconifiable(true);
 		setMaximizable(true);
 		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		getContentPane().setLayout(new GridLayout(0, 2, 2, 4));
 		
 		JLabel lblUsuarios = new JLabel("Usuarios");
 		getContentPane().add(lblUsuarios);
@@ -80,7 +104,26 @@ public class verUsuariosEliminados extends JInternalFrame {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				if(tableUsuarios.getSelectedRow()>0){
-					//cargar datos de tableUsuariosDatos y la de canal
+					PersistenciaCtrl p = new PersistenciaCtrl();
+					DefaultTableModel tm = (DefaultTableModel) tableUsuarios.getModel();
+					Integer dato=Integer.valueOf((String) tm.getValueAt(tableUsuarios.getSelectedRow(),0));
+					Usuario user = p.getInfoUsuario(dato);
+					Canal userC = user.getCanalPropio();
+					//limpiar tablas
+					limpiarTabla(tableCanal);
+					limpiarTabla(tableDatosUsr);
+					//cargar tabla
+					DefaultTableModel modeloTabla= (DefaultTableModel) tableDatosUsr.getModel();
+					modeloTabla.setRowCount(0);
+					modeloTabla.addRow(new Object[]{user.getNickname(),user.getEmail(),user.getNombre(),user.getApellido(),
+							user.getFechaNacimiento()});
+					DefaultTableModel modeloTablaCanal=(DefaultTableModel) tableCanal.getModel();
+					modeloTablaCanal.setRowCount(0);
+					modeloTablaCanal.addRow(new Object[]{userC.getIdCanal(),userC.getNombre(),userC.getDescripcion(),userC.getPrivacidadCanal()});
+					//cargar jlist
+					Map<String, ListaReproduccion> userL = userC.getListasReproduccion();
+					Map<String, Video> userV = userC.getVideos();
+					
 				}
 			}
 		});
@@ -95,8 +138,25 @@ public class verUsuariosEliminados extends JInternalFrame {
 		tableDatosUsr = new JTable(ModeloUsuarioDatos());
 		getContentPane().add(tableDatosUsr);
 		
+		lblDatosCanal = new JLabel("Datos Canal");
+		getContentPane().add(lblDatosCanal);
+		
 		tableCanal = new JTable();
 		getContentPane().add(tableCanal);
+		
+		lblListas = new JLabel("Listas");
+		getContentPane().add(lblListas);
+		
+		JLabel lblVideos = new JLabel("Videos");
+		getContentPane().add(lblVideos);
+		
+		listListas = new JList();
+		listListas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		getContentPane().add(listListas);
+		
+		listVideos = new JList();
+		listVideos.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		getContentPane().add(listVideos);
 
 	}
 	public void cargarUsuarios(){
