98a908d34394d07bc9fbb73df8f03aa2ae8b597f
maria.usuca <maria.usuca@pcunix81.fing.edu.uy>
2018-11-15 13:52:20 -0300

    se ve mas lindo

--- GUI/src/uytube/admin/usuarios/VerInfoListaEliminada.java
+++ GUI/src/uytube/admin/usuarios/VerInfoListaEliminada.java
@@ -15,15 +15,21 @@ import javax.swing.table.DefaultTableModel;
 import uyTubePersistencia.ListaReproduccion;
 import uyTubePersistencia.Video;
 import uytube.admin.adminPrincipal;
+import uytubeLogic.logica.ListaReproduccion.TipoLista;
+import uytubeLogic.logica.SystemHandler.Privacidad;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class VerInfoListaEliminada extends JInternalFrame {
-	private JTable tableDatosLista;
 	private JTable tableVideos;
 	private Map<Integer, Video> userV;
+	private JTextField textFieldNombre;
+	private JTextField textFieldTipo;
+	private JTextField textFieldprivacidad;
+	private JTextField textFieldId;
 	
 	DefaultTableModel ModeloLista() 
 	{
@@ -54,12 +60,38 @@ public class VerInfoListaEliminada extends JInternalFrame {
 		
 		JPanel panel = new JPanel();
 		getContentPane().add(panel);
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
 		
 		JLabel lblDatosLista = new JLabel("Datos Lista");
 		panel.add(lblDatosLista);
 		
-		tableDatosLista = new JTable(ModeloLista());
-		panel.add(tableDatosLista);
+		JLabel Id = new JLabel("Id:");
+		panel.add(Id);
+		
+		textFieldId = new JTextField();
+		textFieldId.setEditable(false);
+		panel.add(textFieldId);
+		
+		JLabel label = new JLabel("Nombre:");
+		panel.add(label);
+		
+		textFieldNombre = new JTextField();
+		textFieldNombre.setEditable(false);
+		panel.add(textFieldNombre);
+		
+		JLabel lblTipo = new JLabel("Tipo:");
+		panel.add(lblTipo);
+		
+		textFieldTipo = new JTextField();
+		textFieldTipo.setEditable(false);
+		panel.add(textFieldTipo);
+		
+		JLabel lblPrivacidad = new JLabel("Privacidad:");
+		panel.add(lblPrivacidad);
+		
+		textFieldprivacidad = new JTextField();
+		textFieldprivacidad.setEditable(false);
+		panel.add(textFieldprivacidad);
 		
 		JPanel panel_1 = new JPanel();
 		getContentPane().add(panel_1);
@@ -74,10 +106,10 @@ public class VerInfoListaEliminada extends JInternalFrame {
 					DefaultTableModel tm = (DefaultTableModel) tableVideos.getModel();
 					Integer idVideo= (Integer) tm.getValueAt(tableVideos.getSelectedRow(),0);
 					Video videoE = userV.get(idVideo);
-					VerInfoVideoEliminado videoIFrame = new VerInfoVideoEliminado(videoE);
-					adminPrincipal.getFrames()[0].setLayout(null);
-					adminPrincipal.getFrames()[0].add(videoIFrame);
-					videoIFrame.show();
+				//	VerInfoVideoEliminado videoIFrame = new VerInfoVideoEliminado(videoE);
+				//	adminPrincipal.getFrames()[0].setLayout(null);
+				//	adminPrincipal.getFrames()[0].add(videoIFrame);
+				//	videoIFrame.show();
 				}
 			}
 		});
@@ -91,8 +123,21 @@ public class VerInfoListaEliminada extends JInternalFrame {
 	}
 	
 	public void cargarDatos(ListaReproduccion lista){
-		DefaultTableModel modeloDatosLista= (DefaultTableModel) tableDatosLista.getModel();
-		modeloDatosLista.addRow(new Object[]{lista.getIdListaRep(),lista.getNombre(),lista.getTipo(), lista.getPrivado()});
+
+		textFieldId.setText(Integer.toString(lista.getIdListaRep()));
+		textFieldNombre.setText(lista.getNombre());
+		if(lista.getTipo() == TipoLista.PARTICULAR){
+			textFieldTipo.setText("Particular");
+		}else{
+			textFieldTipo.setText("Por defecto");
+		}
+		if(lista.getPrivado() == Privacidad.PRIVADO){
+			textFieldprivacidad.setText("Privado");
+		}else{
+			textFieldprivacidad.setText("Publico");
+		}
+		
+		
 		DefaultTableModel modeloVideos= (DefaultTableModel) tableVideos.getModel();
 		userV = lista.getVideos();
 		for (Entry<Integer, Video> entryV : userV.entrySet()) {
